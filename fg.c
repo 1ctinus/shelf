6edc858aa4b9e12c0dfab7ebe106e1d43c0a562f
1ctinus <72636637+1ctinus@users.noreply.github.com>
2021-01-13 13:15:18 -0600

    seeds and stuff; truly random

--- fg.c
+++ fg.c
@@ -1,18 +1,33 @@
 #include <math.h>
 #include <stdio.h>
-#include <stdlib.h> 
-void calc(float x){
-int rando = rand() % 100;
-float odds = 99 * exp ( -1*pow( (( 17.5 - x ) / 50) ,2) ) - exp( (1.65*x-78.875) /8);
-/*char good[];*/
-if( rando < odds ) printf("KICK IS GOOD!!\n"); else printf("KICK IS NO GOOD!!\n");
+#include <stdlib.h>
+#include <time.h>
+void calc(float x)
+{
+    srand(time(NULL));   // Initialization, should only be called once.
+    int rando = rand() % 100;
+    float odds = 99 * exp(-1 * pow(((17.5 - x) / 50), 2)) - exp((1.65 * x - 78.875) / 8);
+    /*char good[];*/
+    printf("%f", odds);
+    printf("\n");
+        printf("%d", rando);
+    if (rando < odds)
+        printf("KICK IS GOOD!!\n");
+    else
+        printf("KICK IS NO GOOD!!\n");
 };
-int main(){ 
-float inp;
-printf("DISTANCE: ");
-scanf("%f",&inp);
-if(inp > 17 && inp < 70){
-calc(inp);
-}else {printf("DISTANCE IS INVALID.");};
-return 0;
+int main()
+{
+    float inp;
+    printf("DISTANCE: ");
+    scanf("%f", &inp);
+    if (inp > 17 && inp < 70)
+    {
+        calc(inp);
+    }
+    else
+    {
+        printf("DISTANCE IS INVALID.");
+    };
+    return 0;
 }

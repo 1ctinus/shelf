62b6ecd43034255af0d8d3b54aee223fcd0119be
Ict <72636637+1ctinus@users.noreply.github.com>
2021-02-11 18:25:18 -0600

    ThIs iS A GiT CoMmIt

--- meme.c
+++ meme.c
@@ -1,21 +1,19 @@
-/* Demonstrate toupper and tolower for 
-   standard C strings.
-   This does not work for multibyte character sets. */
 #include <ctype.h>
 #include <stdio.h>
+#include <unistd.h>
 
-void mock(char *s){
-
-    while(*s){
-        *s=toupper(*s);
-        s = s + 2;
+int main(int argc, char *argv[])
+{
+    char t[32767];
+    read(STDIN_FILENO, t, 32767);
+    int i;
+    for (i = 0; t[i] != '\0'; i = i + 2)
+    {
+        if (t[i] >= 'a' && t[i] <= 'z' && t[(i-1)] != '\\')
+        {
+            t[i] = t[i] - 32;
+        }
     }
-}
-
-int main(int argc, char *argv[]){
-    char t[255];
-    scanf("%[^\t\n]",t);
-    mock(t);
     printf("%s\n", t);
     return 0;
 }
